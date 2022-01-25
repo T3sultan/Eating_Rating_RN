f13d4c4a98f656bb815672416db36febf43529b6
“T3sultan” <incognito.soul011@gmail.com>
2022-01-26 01:08:42 +0600

    categories item

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,39 +1,64 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
 import React from 'react';
+import { ScrollView } from 'react-native';
 
 const items = [
       {
-            image:require("../images/categories_images/shopping-bag.png"),
-            text:'Pick Up'
+            image: require("../images/categories_images/shopping-bag.png"),
+            text: 'Pick-up'
       },
       {
-            image:require("../images/categories_images/soft-drink.png"),
-            text:'soft-drink'
+            image: require("../images/categories_images/soft-drink.png"),
+            text: 'Soft-Drink'
       },
       {
-            image:require("../images/categories_images/fast-food.png"),
-            text:'fast-food'
+            image: require("../images/categories_images/fast-food.png"),
+            text: 'Fast-Food'
       },
       {
-            image:require("../images/categories_images/desserts.png"),
-            text:'desserts'
+            image: require("../images/categories_images/desserts.png"),
+            text: 'Fesserts'
       },
       {
-            image:require("../images/categories_images/deals.png"),
-            text:'deals'
+            image: require("../images/categories_images/deals.png"),
+            text: 'Deals'
       },
       {
-            image:require("../images/categories_images/coffee.png"),
-            text:'coffee'
+            image: require("../images/categories_images/coffee.png"),
+            text: 'Coffee'
       },
 
 ]
 
 export default function Categories() {
       return (
-            <View>
-                  <Text>Categories</Text>
+            <View style={{ marginTop: 5, backgroundColor: "white", paddingHorizontal: 10, paddingLeft:10}}>
+                  <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+                        {
+                              items.map((item, index) => (
+                                    <View key={index} style={{ alignItems: 'center', marginRight: 20 }}>
+                                          <Image
+                                                source={item.image}
+                                                style={{
+                                                      width: 50,
+                                                      height: 40,
+                                                      resizeMode: "contain",
+                                                      borderRadius: 10
+                                                }}
+                                          />
+                                          <Text style={{ fontSize: 12, fontWeight: "bold" }}>{item.text}</Text>
+
+                                    </View>
+
+                              ))
+                        }
+
+
+
+                  </ScrollView>
+
             </View>
+
       );
 }
 
